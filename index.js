f8e50bf9a5c9cce2f6753121a576c6dcfd27c1ac
Osama daoud <osamadaoud66@gmail.com>
2024-01-09 05:14:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function window(customerName){
+return window();
+}
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+  }
+
+  function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+  }
+
+  const leastFavoriteCustomer=  "osama" ; 
+   
+  function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "bob"
+  }
